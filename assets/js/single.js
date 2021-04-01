c124e08b4530d26f774cf38339a6cdc137613845
jermz678 <jermz678@live.com>
2021-03-31 21:23:32 -0500

    took care of errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,21 +1,62 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else{
+        document.location.replace("./index.html");
+    }
+};
+
+
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+  
+
+
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response) {
-        // request was successful
-        if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data);
-          });
-        }
-        else {
-          alert("There was a problem with your request!");
+    // make a get request to url
+fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
+  
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
         }
       });
-  };
-  getRepoIssues("facebook/react");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
+    }
+  });
+}  
+      
+  
+ 
 
 
   var displayIssues = function(issues) {
@@ -51,3 +92,4 @@ var getRepoIssues = function(repo) {
         issueContainerEl.appendChild(issueEl);
         }   
 };
+getRepoName()
